11229fcc3bc1d4836c4268d86b58893e154a3c24
milindchawre <milndchawre@gmail.com>
2022-12-15 16:57:27 +0530

    Making changes in rails host config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,6 @@ Rails.application.config.hosts = [
   IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
   IPAddr.new("::/0"),             # All IPv6 addresses.
   "localhost",                    # The localhost reserved domain.
-  ENV["RAILS_HOSTS"]  # Additional comma-separated hosts for development.
+  ENV["RAILS_HOST1"],  # Additional comma-separated hosts for development.
+  ENV["RAILS_HOST2"]
 ]
